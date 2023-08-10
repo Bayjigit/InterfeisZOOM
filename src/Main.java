a9ad82cc967130cd6d4f63f3c63da9e9012cc7de
user <cleisxsfg@gmail.com>
2023-08-10 17:36:27 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Plan plan= new Plan();
+        System.out.println(plan);
+        plan.canfly();
+
+        Bird bird= new Bird();
+        System.out.println(bird);
+        bird.canfly();
+
+    }
+}
